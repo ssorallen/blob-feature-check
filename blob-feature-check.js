597a09ab2881cb97f0ec58996b4c953d82d8008d
Ross Allen <rossgit@gmail.com>
2014-02-09 10:30:25 -0800

    Detect blob: URL support with an Image
    1. Create an SVG as a string
    2. Pass the SVG as Blob data
    3. Create a URL for the Blob data
    4. Set the blob URL as the "src" of an Image
    
    If onload fires, "blob:" URLs are supported. If onerror fires, "blob:"
    URLs are unsupported.

---
+++ blob-feature-check.js
@@ -0,0 +1,14 @@
+var svg = new Blob(
+  ["<svg xmlns='http://www.w3.org/2000/svg'></svg>"],
+  {type: "image/svg+xml;charset=utf-8"}
+);
+
+var img = new Image();
+var featureDiv = document.getElementById("blob-urls");
+img.onload = function() {
+  featureDiv.className += " feature-supported";
+};
+img.onerror = function() {
+  featureDiv.className += " feature-unsupported";
+};
+img.src = URL.createObjectURL(svg);
