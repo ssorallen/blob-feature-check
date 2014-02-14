3073c540321ed97ed4457db5f52665375bd589d3
Ross Allen <rossgit@gmail.com>
2014-02-13 21:24:10 -0800

    Add version and licensing comment to JS file
    Fixes #2.

--- blob-feature-check.js
+++ blob-feature-check.js
@@ -1,3 +1,10 @@
+/**
+ * Blob Feature Check v1.0.0
+ *
+ * Released into the public domain.
+ * http://unlicense.org
+ */
+
 var svg = new Blob(
   ["<svg xmlns='http://www.w3.org/2000/svg'></svg>"],
   {type: "image/svg+xml;charset=utf-8"}
