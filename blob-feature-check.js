60a3f711224f24ed99672efe3a8442a3cf8b2575
Ross Allen <rossgit@gmail.com>
2014-02-25 08:19:57 -0800

    Test for "blob:" in createObjectURL URLs for support
    If `URL.createObjectURL` returns a URL that starts with something other
    than "blob:", it can be assumed that the `URL` object has been
    polyfilled by a library and there is no need to test further.
    
    This addresses the environment created by Blob.js[1] for Internet
    Explorer and makes this check for "blob:" URL support fail as it should.
    
    [1]: https://github.com/eligrey/Blob.js/

--- blob-feature-check.js
+++ blob-feature-check.js
@@ -1,24 +1,33 @@
 /**
- * Blob Feature Check v1.0.0
+ * Blob Feature Check v1.1.0
  *
  * Released into the public domain.
  * http://unlicense.org
  */
+(function() {
+  var featureDiv = document.getElementById("blob-urls");
 
-var svg = new Blob(
-  ["<svg xmlns='http://www.w3.org/2000/svg'></svg>"],
-  {type: "image/svg+xml;charset=utf-8"}
-);
+  function fail() { featureDiv.className += " feature-unsupported"; }
+  function pass() { featureDiv.className += " feature-supported"; }
 
-var img = new Image();
-var featureDiv = document.getElementById("blob-urls");
-img.onload = function() {
-  featureDiv.className += " feature-supported";
-};
-img.onerror = function() {
-  featureDiv.className += " feature-unsupported";
-};
+  var svg = new Blob(
+    ["<svg xmlns='http://www.w3.org/2000/svg'></svg>"],
+    {type: "image/svg+xml;charset=utf-8"}
+  );
 
-// Safari 6 uses "webkitURL".
-var url = window.webkitURL || window.URL;
-img.src = url.createObjectURL(svg);
+  // Safari 6 uses "webkitURL".
+  var url = window.webkitURL || window.URL;
+  var objectUrl = url.createObjectURL(svg);
+
+  if (/^blob:/.exec(objectUrl) === null) {
+    // `URL.createObjectURL` created a URL that started with something other
+    // than "blob:", which means it has been polyfilled and is not supported by
+    // this browser.
+    fail();
+  } else {
+    var img = new Image();
+    img.onload = function() { pass(); };
+    img.onerror = function() { fail(); };
+    img.src = objectUrl;
+  }
+})();
